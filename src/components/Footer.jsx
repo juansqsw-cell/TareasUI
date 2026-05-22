383ba995fb1c37996d68535d9459d5074b60d87e
Jamer <juansqsw@gmail.com>
2026-05-22 15:22:18 -0500

    Agrega componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Actividad Prueba - Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
